96416fa9581226c68800475f51a1fb7839c0c352
vasile luta <elsile69@yahoo.com>
2019-07-17 15:01:34 +0300

    FizzBuzz

--- index.js
+++ index.js
@@ -58,3 +58,21 @@ console.log(
 let nText = (a, n) => a.repeat(n);
 
 console.log(nText('text', 7));
+
+let fizzBuzz = function() {
+  for(let i = 1; i <= 100; i++) {
+    if((i % 3 === 0) && (i % 5 === 0)) {
+      console.log('FizzBuzz');
+    }
+    else if(i % 3 === 0) {
+      console.log('Fizz');
+    }
+    else if(i % 5 === 0) {
+      console.log('Buzz');
+    }
+    else
+      console.log(i);
+  }
+};
+
+console.log(fizzBuzz());
